f87db9410b22171d2ce0c8c8016eb6bb2ebde475
DSW1609 <DSW7296666@163.com>
2019-07-05 11:11:11 +0800

    7 5 11 11 DSW

--- js/index.js
+++ js/index.js
@@ -6,30 +6,30 @@ function sso(){
 	var sson = document.getElementById("ssimg");
 	sson.src = "images/搜索.png";
 }
-function gyvs(){
-	var gyvs = document.getElementById("triangle");
-	var gyvs2 = document.getElementById("abvsover");
-	gyvs.style.display = "block";
-	gyvs2.style.display = "block";
-}
-function gyvso(){
-	var gyvso = document.getElementById("triangle");
-	var gyvso2 = document.getElementById("abvsover");
-	gyvso.style.display = "none";
-	gyvso2.style.display = "none";
-}
-function remen(){
-	var remen = document.getElementById("triangle2");
-	var remen2 = document.getElementById("remenover");
-	remen.style.display = "block";
-	remen2.style.display = "block";
-}
-function remeno(){
-	var gyvso = document.getElementById("triangle2");
-	var gyvso2 = document.getElementById("remenover");
-	gyvso.style.display = "none";
-	gyvso2.style.display = "none";
-}
+//function gyvs(){
+//	var gyvs = document.getElementById("triangle");
+//	var gyvs2 = document.getElementById("abvsover");
+//	gyvs.style.display = "block";
+//	gyvs2.style.display = "block";
+//}
+//function gyvso(){
+//	var gyvso = document.getElementById("triangle");
+//	var gyvso2 = document.getElementById("abvsover");
+//	gyvso.style.display = "none";
+//	gyvso2.style.display = "none";
+//}
+//function remen(){
+//	var remen = document.getElementById("triangle2");
+//	var remen2 = document.getElementById("remenover");
+//	remen.style.display = "block";
+//	remen2.style.display = "block";
+//}
+//function remeno(){
+//	var gyvso = document.getElementById("triangle2");
+//	var gyvso2 = document.getElementById("remenover");
+//	gyvso.style.display = "none";
+//	gyvso2.style.display = "none";
+//}
 $(function(){
   //下面我们需要开发用户滚动时，当滚动向下一定距离，展示这个隐藏的顶端固定导航
    
